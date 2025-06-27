ae9eaf55bf9b016267b384f15fcf64e14c25b4e3
kovidbehl97 <kovid.behl@gmail.com>
2025-06-27 16:41:00 -0400

    feat(app): :sparkles: Add API route entrypoints Register routes for posts, comments, friends, notifications, messages, and users in app.ts with cookie-parser middleware.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,27 @@
-import express from 'express';
+// friendbook/server/src/app.ts
+import express, { Router, Express } from 'express';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
-
 import authRoutes from './routes/authRoutes';
+import postRoutes from './routes/postRoutes';
+import commentRoutes from './routes/commentRoutes';
+
+import friendRoutes from './routes/friendRoutes';
+import notificationRoutes from './routes/notificationRoutes';
+import userRoutes from './routes/userRoutes';
+import messageRoutes from './routes/messageRoutes';
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
 
 app.use(cors(
   {
-    origin: 'http://localhost:5173',
-    credentials: true,
+    origin: 'http://localhost:5173',  
+    credentials: true,               
   }
 ));
+
 
 app.use(fileUpload({
   useTempFiles: true,
@@ -22,5 +31,11 @@ app.use(fileUpload({
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/friends', friendRoutes);
+app.use('/api/notifications', notificationRoutes);
+app.use('/api/messages', messageRoutes)
+app.use('/api/users', userRoutes);
 
 export default app;
